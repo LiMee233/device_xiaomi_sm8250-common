37b3ca4a26b10ea710f4a70abc867050243b11a5
LiMeea <LiMee233@outlook.com>
2021-12-02 09:22:42 +0800

    lmi: make LED color optional * Available colors are: blue, green, cyan, red, purple, yellow and white, the default value is white. * Also optimized string name, title and Chinese translation.

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraPreferences.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraPreferences.java
@@ -18,7 +18,7 @@ package org.lineageos.settings.popupcamera;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.preference.PreferenceManager; 
 
 public class PopupCameraPreferences {
 
@@ -26,6 +26,8 @@ public class PopupCameraPreferences {
     private static final boolean DEBUG = false;
     private static final String LED_EFFECT_KEY = "popup_led_effect";
     private static final boolean LED_EFFECT_DEFAULT_VALUE = true;
+    private static final String LED_COLOR_KEY = "popup_led_color";
+    private static final String LED_COLOR_DEFAULT_VALUE = "7";
     private static final String SOUND_EFFECT_KEY = "popup_sound_effect";
     private static final String SOUND_EFFECT_DEFAULT_VALUE = "0";
     private SharedPreferences mSharedPrefs;
@@ -34,11 +36,15 @@ public class PopupCameraPreferences {
         mSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
     }
 
-    public String getSoundEffect() {
-        return mSharedPrefs.getString(SOUND_EFFECT_KEY, SOUND_EFFECT_DEFAULT_VALUE);
-    }
-
     public boolean isLedAllowed() {
         return mSharedPrefs.getBoolean(LED_EFFECT_KEY, LED_EFFECT_DEFAULT_VALUE);
+    }
+
+    public String getLEDColor() {
+        return mSharedPrefs.getString(LED_COLOR_KEY, LED_COLOR_DEFAULT_VALUE);
+    }
+
+    public String getSoundEffect() {
+        return mSharedPrefs.getString(SOUND_EFFECT_KEY, SOUND_EFFECT_DEFAULT_VALUE);
     }
 }
