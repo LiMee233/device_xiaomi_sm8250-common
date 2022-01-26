e12c98fc479875daac13e6bd221dbd2c8e43291b
Zinadin Zidan <zidan44@pixelexperience.org>
2022-01-27 00:31:00 +0800

    sm8250-common: dependencies: Track sm8250-extra repo * platform-bootclasspath cannot depend on something from a non-root soong namespace. * This fix Alipay fingerprint payment.
    TEST: Build and Alipay fp payment works well.

--- kona.mk
+++ kona.mk
@@ -292,8 +292,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
 
 # IPACM
 PRODUCT_PACKAGES += \
