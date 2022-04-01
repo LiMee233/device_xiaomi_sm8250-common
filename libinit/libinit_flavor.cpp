5e9f758c513c1e352e502f60376570c4c9e24352
LiMee233 <LiMee233@outlook.com>
2022-04-01 14:34:41 +0800

    sm8250-common: Override ro.build.flavor [1/2] ** For some Chinese college students only **
    * set ro.build.flavor from $TARGET_BUILD_FLAVOR to
      $TARGET_DEVICE-$TARGET_BUILD_TYPE. For example: aosp_lmi-user ->
      lmi-user.
    * Some application treat device with "aosp" in ro.build.flavor as
      an Android emulator and refuses to run. Now let's circumvent this
      detection.

---
+++ libinit/libinit_flavor.cpp
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/properties.h>
+#include <libinit_utils.h>
+
+#include <libinit_flavor.h>
+
+using android::base::GetProperty;
+
+#define FLAVOR_PROP "ro.build.flavor"
+#define TARGET_DEVICE_PROP "ro.build.product"
+#define TARGET_BUILD_TYPE_PROP "ro.build.type"
+
+void set_flavor_prop() {
+    std::string targetBuildType = GetProperty(TARGET_BUILD_TYPE_PROP, "");
+    std::string targetDevice = GetProperty(TARGET_DEVICE_PROP, "");
+    property_override(FLAVOR_PROP, targetDevice + "-" + targetBuildType);
+}
