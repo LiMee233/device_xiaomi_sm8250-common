2faf0fb5ce02bcd6bf285847f87317d5d4b9556d
LiMee233 <LiMee233@outlook.com>
2022-05-01 21:57:38 +0800

    sm8250-common: Override ro.build.flavor [1/2] * Use set_flavor_prop(setCustomFlavor, customFlavor):   setCustomFlavor: bool, ro.build.flavor will be set to   $TARGET_DEVICE-$TARGET_BUILD_TYPE (such as alioth-user) if false;   will set to customFlavor if true. * customFlavor: std::string, used when setCustomFlavor set to true.
    * Some application treat device with "aosp" in ro.build.flavor as
      an Android emulator and refuses to run. Now let's circumvent this
      detection.

---
+++ libinit/include/libinit_flavor.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LIBINIT_FLAVOR_H
+#define LIBINIT_FLAVOR_H
+
+#include <string>
+
+void set_flavor_prop(bool setCustomFlavor, const std::string &customFlavor);
+
+#endif // LIBINIT_FLAVOR_H
